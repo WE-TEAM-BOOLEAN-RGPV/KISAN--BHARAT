7929721094df623a929b9a1703ea303bac8d1656
Mayank Rai <mayankraivns@gmail.com>
2023-11-07 19:19:29 +0530

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,32 +1,141 @@
-import { listen } from "./app";
-import connectDatabase from "./db/Database";
-//import app from './app';
-//query for database connectivity
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = 5174
+const cors = require('cors')
+const { error } = require('console')
+const {run1, run2, run3, run4} = require('./model/mongodb')
+const { json } = require('stream/consumers')
 
-//handling uncaught Exceptions
-process.on("uncaughtException", (err)=>{
-    console.log(`Error : ${err.message}`);
-    console.log('shutting down the server for handling uncaught exception in code ')
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+
+const vendor_data = async() => {
+    let vend = await run1();
+    vend = await collv.find().toArray();
+    return(vend);
+}
+let vendors = vendor_data();
+
+
+const farmer_data = async() => {
+    let farm_d = await run2()
+    farm_d = await collf.find().toArray();
+    return(farm_d);
+}
+let farmers = farmer_data()
+
+
+const Pbuy_data = async() => {
+    let pbdata = await run3()
+    pbdata = await collb.find().toArray();
+    return(pbdata);
+}
+let buy = Pbuy_data()
+
+
+const Prent_data = async() => {
+    let prdata = await run4()
+    prdata = await collr.find().toArray();
+    return(prdata);
+}
+let rent = Prent_data()
+
+
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'./view/index.html'))
 })
 
-//config
-if(process.env.NODE_ENV !== "Production"){
-    require("dotenv").config({
-        path:"backend/config/.env"
+
+app.get('/api/farmers', async(req, res) => {
+
+    const asyncOperation = () => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(farmers);
+          }, 1000);
+        });
+      };
+    asyncOperation()
+    .then((farmers) => {
+      res.json(farmers);
     })
-}
-//create server
-const server = listen(process.env.PORT,()=>{
-    console.log(`Server is Running on the http://localhost:${process.env.PORT}`);
-});
-
-connectDatabase();
-
-//Unhandled promise rejection
-process.on("unhandledRejection" ,(err) =>{
-    console.log(`Shutting down the Server for ${err.message}`);
-    console.log("Shutting down the Server for unhandled promise rejection");
-    server.close(()=>{
-        process.exit(1);
+    .catch((err) => {
+      res.status(500).json({ error: 'Error encountered!' });
     });
-});
+
+    console.log(farmers)
+    })
+
+
+app.get('/api/vendors', async(req, res) => {
+
+    const asyncOperation = () => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(vendors);
+          }, 1000);
+        });
+      };
+    asyncOperation()
+    .then((vendors) => {
+      res.json(vendors);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Error encountered!' });
+    });
+
+    console.log(vendors)
+    })
+
+
+
+app.get('/api/product_buy', async(req, res) => {
+
+    const asyncOperation = () => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(buy);
+          }, 1000);
+        });
+      };
+    asyncOperation()
+    .then((buy) => {
+      res.json(buy);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Error encountered!' });
+    });
+
+    console.log(buy)
+    })
+
+
+
+app.get('/api/product_rent', async(req, res) => {
+
+    const asyncOperation = () => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(rent);
+          }, 1000);
+        });
+      };
+    asyncOperation()
+    .then((rent) => {
+      res.json(rent);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Error encountered!' });
+    });
+
+    console.log(rent)
+    })
+
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
