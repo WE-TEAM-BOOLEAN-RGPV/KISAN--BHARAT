d47f7495a20ff4bad44629355b6cceb6a00f5c2a
Mayank <mayankraivns@gmail.com>
2023-11-16 14:38:01 +0530

    update

--- Backend/controller/data.js
+++ Backend/controller/data.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose')
 const {farmers, vendors, prod_buy, prod_rent} = require('../model/user');
 const { error } = require('console');
   
-const create_farm = (req,res) => {
-    const new_farm = new farmers(req.body);
-        new_farm.save()
-        .then((doc) => {
-            console.log(doc);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+const create_farm = async(req,res) => {
+    try {
+        const new_farmer = new farmers(req.body);
+        await new_farmer.save();
+        res.status(201).json({ message: 'Data saved successfully' });
+        
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error! data not saved!' });
+      }
 }
 
 module.exports = {
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const urouter = express.Router()
+const router = express.Router()
 const {farm_data, vendor_data, prod_buy_data, prod_rent_data} = require('../controller/user')
 
-urouter
+router
   .get('/farmers', farm_data)
   .get('/vendors', vendor_data)
   .get('/prod_buy', prod_buy_data)
   .get('/prod_rent', prod_rent_data);
 
-  exports.urouter = urouter;
+  exports.urouter = router;
