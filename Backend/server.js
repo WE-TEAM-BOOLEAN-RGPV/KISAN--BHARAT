9038cc5b305a4b37c20538f169161ed5883a3455
Mayank <mayankraivns@gmail.com>
2023-11-16 17:47:23 +0530

    MOD

--- Backend/server.js
+++ Backend/server.js
@@ -5,21 +5,26 @@ const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const get_router = require('./routes/user')
-const post_router = require('./routes/data')
+const user_router = require('./routes/user')
+const app_job_router = require('./routes/app_job')
 const bcrypt = require('bcrypt')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require("method-override")
 
+app
+  .use(express.json({ extended: false }))
+  .use(cors())
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }));
 
-app.use(express.json({ extended: false }))
-app.use(cors())
-app.use(bodyParser.json())
-app.use(express.urlencoded({ extended: false }))
-
-app.use('/user', get_router.urouter);
-app.use('/data', post_router.drouter);
+app
+  .use('/farmers', user_router.farmer_router)
+  .use('/vendors', user_router.vendor_router)
+  .use('/buy_prod', user_router.buy_prod_router)
+  .use('/rent_prod', user_router.rent_prod_router)
+  .use('/fpo_app', app_job_router.fpo_router)
+  .use('/job_list', app_job_router.job_router)
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, "./view/login.html"));
